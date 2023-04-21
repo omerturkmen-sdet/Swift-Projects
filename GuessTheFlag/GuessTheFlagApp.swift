f751769eb015da4fc2beef5daafacc3db00ae6d0
omerturkmen <omerturkmen.sdet@gmail.com>
2023-04-21 17:02:54 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by ömer türkmen on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
